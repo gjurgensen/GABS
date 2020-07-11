838729511c7f79086f01a9ee96c958b466c6c045
Grant Jurgensen <gajurgensen@gmail.com>
2020-07-10 21:39:27 -0500

    Define Show Exp

--- src/GabsAst.hs
+++ src/GabsAst.hs
@@ -2,7 +2,7 @@ module GabsAst where
 
 type Name = String
 
-data Exp = 
+data Exp =
     B Bool
   | I Integer
   | Lambda Name Type Exp
@@ -18,6 +18,24 @@ data Exp =
   | Ite Exp Exp Exp
   | App Exp Exp
   deriving Eq
+
+instance Show Exp where
+  show exp = case exp of
+    B b -> show b
+    I i -> show i
+    Lambda n t e -> "λ " ++ n ++ ": " ++ show t ++ ". " ++ show e
+    Var n -> n
+    And e1 e2 -> parens $ show e1 ++ " and " ++ show e2
+    Or e1 e2 -> parens $ show e1 ++ " or " ++ show e2
+    Not e -> parens $ show e
+    Plus e1 e2 -> parens $ show e1 ++ " + " ++ show e2
+    Minus e1 e2 -> parens $ show e1 ++ " - " ++ show e2
+    Times e1 e2 -> parens $ show e1 ++ " * " ++ show e2
+    Div e1 e2 -> parens $ show e1 ++ " / " ++ show e2
+    Ite e1 e2 e3 -> "if " ++ show e1 ++ " then " ++ show e2 ++ " else " ++ show e3
+    App e1 e2 -> parens (show e1) ++ " " ++ show e2
+    where
+      parens s = "(" ++ s ++  ")"
 
 data Type =
     TBool
